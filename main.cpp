cb6a941a759cfd036f544b6a4741a30566dee6f8
maxgonick <maxwellgonick@g.ucla.edu>
2022-03-06 19:07:02 -0800

    Updated Test Cases

--- main.cpp
+++ main.cpp
@@ -11,17 +11,27 @@ int main(){
     BloodDonation doner1( 752401, 45, 99.56);
     BloodDonation doner2(889543, 55, 109.50);
     BloodDonation doner3 (89643244, 65, 187.55);
-    BloodDonation doner4 (855565, 17, 127.00);
     VacationAccount account (889543);
     assert( std::to_string(doner1.getID()) == "752401");
     assert(std::to_string(doner2.getAge()) == "55");
-//    assert(std::to_string(doner3.getWeight()) == "187.55");
-    assert(std::to_string(account.getBalance()) == "0.000000");
-    assert(std::to_string(account.getID()) == "889543");
-    assert(account.addVacationToAccount(doner2) == true);
-    assert(account.addVacationToAccount(doner1) == false);
-    assert(std::to_string(account.getBalance()) == "4.000000");
-    cout << "i love josh and cael and jason lols! \n";
-
+    assert(std::to_string(doner3.getWeight()) == "187.550000");
+    BloodDonation donor1( 752401, 45, 99.56);
+    BloodDonation donor2( 889543, 55, 109.50);
+    BloodDonation donor3( 89643244, 65, 187.55);
+    BloodDonation donor4( 855565, 17, 127.00);
+    assert(std::to_string(donor1.getID( )) == "752401");
+    assert(std::to_string(donor2.getID( )) == "889543");
+    assert(std::to_string(donor3.getID()) == "-1");
+    assert(std::to_string(donor4.getID( )) == "855565");
+    assert(std::to_string(donor2.getAge( )) == "55");
+    assert(std::to_string(donor4.getAge( )) == "-1");
+    assert(std::to_string(donor3.getWeight( )) == "187.550000");
+    assert(std::to_string(donor1.getWeight( )) == "-1.000000");
+    assert( std::to_string(account.getBalance( ) ) == "0.000000" );
+    assert( std::to_string(account.getID( ) ) == "889543" );
+    assert( account.addVacationToAccount( donor2 ) == true );
+    assert( account.addVacationToAccount( donor1 ) == false );
+    assert( std::to_string( account.getBalance( ) ) == "4.000000" );
+    cout << "all tests passed!";
 }
 
